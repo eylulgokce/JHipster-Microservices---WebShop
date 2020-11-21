abaed7d73145cc1aa4ec6275a740139dbc86c4a5
eylulgokce <44869816+eylulgokce@users.noreply.github.com>
2020-11-21 18:50:17 +0100

    Services are created

---
+++ Project/Microservices/ProductService/Database/IProductdatabase.cs
@@ -0,0 +1,14 @@
+﻿using ProductService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Database
+{
+    public interface IProductDatabase
+    {
+        IEnumerable<Product> GetAllProducts();
+    }
+}
+
