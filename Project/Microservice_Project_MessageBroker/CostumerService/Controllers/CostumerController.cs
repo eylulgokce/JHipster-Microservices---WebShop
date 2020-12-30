25cedf9c673b616fa8ac691985fbc276517aaaac
eylulgokce <44869816+eylulgokce@users.noreply.github.com>
2020-12-30 11:00:48 +0100

    corrected customer controller

--- Project/Microservice_Project_MessageBroker/CostumerService/Controllers/CostumerController.cs
+++ Project/Microservice_Project_MessageBroker/CostumerService/Controllers/CostumerController.cs
@@ -1,4 +1,7 @@
 ﻿using CostumerService.Database;
+using CostumerService.Model;
+using MicroserviceCommon.Clients.Interfaces;
+using MicroserviceCommon.ErrorHandling;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -9,12 +12,29 @@ namespace CostumerService.Controllers
     public class CostumerController : ControllerBase
     {
         private ICostumerDatabase _costumerDatabase;
+        private readonly INotificationsManager _notificationServiceClient;
 
-        public CostumerController(ICostumerDatabase costumerDatabase)
+        public CostumerController(ICostumerDatabase costumerDatabase, INotificationsManager notificationServiceClient)
         {
             _costumerDatabase = costumerDatabase;
+            _notificationServiceClient = notificationServiceClient;
         }
 
-      
+        [HttpPost]
+        public IActionResult AddCustomer([FromBody] Costumer costumer)
+        {
+            try
+            {
+                _costumerDatabase.insertCostumer(costumer.Firstname, costumer.Surname, costumer.Email, costumer.Address, costumer.City, costumer.Country);
+                _notificationServiceClient.PublishNotificationInfo($"Customer {costumer.Firstname} {costumer.Surname} was successfully inserted!");
+            }
+            catch (BaseMicroserviceException ex)
+            {
+                return ex.ToActionResult();
+            }
+
+            return new OkResult();
+        }
+
     }
 }
