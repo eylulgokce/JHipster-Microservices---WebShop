be5f4b46333d1d8702910ac6706abca00feded85
eylulgokce <44869816+eylulgokce@users.noreply.github.com>
2021-01-09 17:30:18 +0100

    implemented client structure

---
+++ Project/Microservice_Project_MessageBroker/TestClient/AbstractTestPublisher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    public abstract class AbstractTestPublisher
+    {
+        public void Start()
+        {
+            _runningTask = Task.Run(Run);
+        }
+
+        public void WaitForEnd()
+        {
+            _runningTask.Wait();
+        }
+
+        protected abstract void Run();
+        private Task _runningTask;
+    }
+}
---
+++ Project/Microservice_Project_MessageBroker/TestClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using MicroserviceCommon.Clients;
+using MicroserviceCommon.CommonModel.Order;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ENTER key to start test...");
+            Console.ReadLine();
+
+            var publishers = new List<AbstractTestPublisher>();
+            publishers.Add(new RandomOrderPublisher(500, TimeSpan.FromMilliseconds(100)));
+            publishers.Add(new CartServicePublisher(500, TimeSpan.FromMilliseconds(100)));
+
+            foreach (var publisher in publishers)
+            {
+                publisher.Start();
+            }
+
+            foreach (var publisher in publishers)
+            {
+                publisher.WaitForEnd();
+            }
+
+            Console.WriteLine("All publishers have ended, press ENTER key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
